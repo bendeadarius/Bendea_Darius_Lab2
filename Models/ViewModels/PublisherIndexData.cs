c56c4a86c3e934131a1b01ac46f9e32df3d818b1
Darius Bendea <bendeadarius3@gmail.com>
2025-11-15 11:15:08 +0200

    lab4 pana la punctul 6

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Bendea_Darius_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
