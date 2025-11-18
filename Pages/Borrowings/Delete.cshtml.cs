d9739f195f630a12fc58de3b96bbe3b5be757b75
Darius Bendea <bendeadarius3@gmail.com>
2025-11-18 22:38:57 +0200

    Dupa rezolvarea sarcinilor din lab

--- Pages/Borrowings/Delete.cshtml.cs
+++ Pages/Borrowings/Delete.cshtml.cs
@@ -29,7 +29,12 @@ namespace Bendea_Darius_Lab2.Pages.Borrowings
                 return NotFound();
             }
 
-            var borrowing = await _context.Borrowing.FirstOrDefaultAsync(m => m.ID == id);
+           
+            var borrowing = await _context.Borrowing
+                .Include(b => b.Book)
+                .ThenInclude(b => b.Author)
+                .Include(b => b.Member)
+                .FirstOrDefaultAsync(m => m.ID == id);
 
             if (borrowing == null)
             {
